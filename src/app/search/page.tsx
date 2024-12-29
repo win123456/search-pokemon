1899da826d7a3e53703b466814e02ba19117f73a
Administrator <chirateep.win@gmail.com>
2024-12-29 21:02:33 +0700

    Fix build error

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,25 @@
+"use client";
 import SearchInput from "../../components/SearchInput";
 import PokemonResult from "../../components/PokemonResult";
 import { Container, Box } from "@mui/material";
+import { Suspense } from "react";
+
+function SearchWrapper() {
+  return (
+    <>
+      <SearchInput />
+      <PokemonResult />
+    </>
+  );
+}
 
 export default function SearchPage() {
   return (
     <Container>
       <Box sx={{ mt: 4 }}>
-        <SearchInput />
-        <PokemonResult />
+        <Suspense fallback={<div>Loading search...</div>}>
+          <SearchWrapper />
+        </Suspense>
       </Box>
     </Container>
   );
